284c489a166cfb75b8117c9177d13b4ea353f937
gsathish1987 <gsathish.k1987@gmail.com>
2022-04-20 21:05:19 +0530

    git practice

---
+++ src/test/java/org/practice/GitPractice.java
@@ -0,0 +1,16 @@
+package org.practice;
+
+public class GitPractice {
+	
+	public static void main(String[] args) {
+		
+		int a =5;
+		int b =5;
+		int c =5;
+		
+		System.out.println(a+c);
+		
+		
+	}
+
+}
